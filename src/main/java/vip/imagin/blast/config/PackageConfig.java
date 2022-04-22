c9b751ceb1bd673e842602ebee4677aebf37225d
wendong <2911890696@qq.com>
2022-04-22 09:25:05 +0800

    删掉所有

--- src/main/java/vip/imagin/blast/config/PackageConfig.java
+++
@@ -1,9 +0,0 @@
-package vip.imagin.blast.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = { "vip.imagin.blast.modules.user.dao"})
-public class PackageConfig {
-}
